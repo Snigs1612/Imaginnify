99f4fff1423a05af029462e2520e6000f2d965a3
Snigs1612 <121951862+Snigs1612@users.noreply.github.com>
2024-10-20 13:56:59 +0530

    initial commit

---
+++ app/(root)/transformations/[id]/add/[type]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddTransformationsTypePage = () => {
+  return (
+    <div>
+      AddTransformationsTypePage
+    
+    </div>
+  )
+}
+
+export default AddTransformationsTypePage
---
+++ lib/utils.ts
@@ -0,0 +1,143 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-prototype-builtins */
+import { type ClassValue, clsx } from "clsx";
+import qs from "qs";
+import { twMerge } from "tailwind-merge";
+
+import { aspectRatioOptions } from "@/constants";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+// ERROR HANDLER
+export const handleError = (error: unknown) => {
+  if (error instanceof Error) {
+    console.error(error.message);
+    throw new Error(`Error: ${error.message}`);
+  } else if (typeof error === "string") {
+    console.error(error);
+    throw new Error(`Error: ${error}`);
+  } else {
+    console.error(error);
+    throw new Error(`Unknown error: ${JSON.stringify(error)}`);
+  }
+};
+
+// PLACEHOLDER LOADER - while image is transforming
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#7986AC" offset="20%" />
+      <stop stop-color="#68769e" offset="50%" />
+      <stop stop-color="#7986AC" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#7986AC" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : btoa(str);
+
+export const dataUrl = `data:image/svg+xml;base64,${toBase64(
+  shimmer(1000, 1000)
+)}`;
+
+// FORM URL QUERY
+export const formUrlQuery = ({
+  searchParams,
+  key,
+  value,
+}: FormUrlQueryParams) => {
+  if (typeof window === "undefined") return "";
+  const params = { ...qs.parse(searchParams.toString()), [key]: value };
+  return `${window.location.pathname}?${qs.stringify(params, { skipNulls: true })}`;
+};
+
+// REMOVE KEY FROM QUERY
+export function removeKeysFromQuery({
+  searchParams,
+  keysToRemove,
+}: RemoveUrlQueryParams) {
+  const currentUrl = qs.parse(searchParams.toString());
+
+  keysToRemove.forEach((key) => {
+    delete currentUrl[key];
+  });
+
+  Object.keys(currentUrl).forEach(
+    (key) => currentUrl[key] == null && delete currentUrl[key]
+  );
+
+  return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
+}
+
+// DEBOUNCE
+export const debounce = (func: (...args: any[]) => void, delay: number) => {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+  return (...args: any[]) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(null, args), delay);
+  };
+};
+
+// GET IMAGE SIZE
+export type AspectRatioKey = keyof typeof aspectRatioOptions;
+export const getImageSize = (
+  type: string,
+  image: any,
+  dimension: "width" | "height"
+): number => {
+  if (type === "fill") {
+    return (
+      aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] || 1000
+    );
+  }
+  return image?.[dimension] || 1000;
+};
+
+// DOWNLOAD IMAGE
+export const download = (url: string, filename: string) => {
+  if (!url) {
+    throw new Error("Resource URL not provided! You need to provide one");
+  }
+
+  fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const blobURL = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = blobURL;
+      if (filename) a.download = `${filename.replace(" ", "_")}.png`;
+      document.body.appendChild(a);
+      a.click();
+    })
+    .catch(handleError);
+};
+
+// DEEP MERGE OBJECTS
+export const deepMergeObjects = (obj1: any, obj2: any) => {
+  if (obj2 == null) return obj1;
+
+  const output = { ...obj2 };
+
+  for (const [key, value] of Object.entries(obj1)) {
+    if (
+      typeof value === "object" &&
+      value &&
+      typeof obj2[key] === "object" &&
+      obj2[key]
+    ) {
+      output[key] = deepMergeObjects(value, obj2[key]);
+    } else {
+      output[key] = value;
+    }
+  }
+
+  return output;
+};
